d6391861935799bb4311e96f10df9f8330eda3b8
Willner Elias Severino <willnerseverino343@gmail.com>
2025-11-20 14:28:24 -0400

    CambiarContrasena+loginFrontend

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,13 @@
 import { api } from "./axiosConfig";
 
 export const authService = {
-  login: (data) => api.post("/auth/login", data),
+  login: async (credentials) => {
+    const { data } = await api.post("/auth/login", credentials);
+    return data;
+  },
+
+  cambiarPassword: async (payload) => {
+    const { data } = await api.post("/auth/cambiar-contrasena", payload);
+    return data;
+  }
 };
